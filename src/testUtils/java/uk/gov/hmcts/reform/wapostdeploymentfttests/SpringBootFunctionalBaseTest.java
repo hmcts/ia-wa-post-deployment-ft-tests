ac767f91bf39c42c0a0e51e771eb8a2cfbab9af9
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-05-14 16:27:34 +0100

    Increase timeout

--- src/testUtils/java/uk/gov/hmcts/reform/wapostdeploymentfttests/SpringBootFunctionalBaseTest.java
+++ src/testUtils/java/uk/gov/hmcts/reform/wapostdeploymentfttests/SpringBootFunctionalBaseTest.java
@@ -12,7 +12,7 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("functional")
 public abstract class SpringBootFunctionalBaseTest {
 
-    public static final int DEFAULT_TIMEOUT_SECONDS = 60;
+    public static final int DEFAULT_TIMEOUT_SECONDS = 120;
     public static final int DEFAULT_POLL_INTERVAL_SECONDS = 5;
     @Autowired
     protected ObjectMapper objectMapper;
