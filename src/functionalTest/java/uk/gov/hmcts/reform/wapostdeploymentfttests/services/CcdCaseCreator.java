8ad2a73a8bbe93d79d608a4121ba02765fce4b01
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-08-11 09:01:05 +0100

    Add CCD Case document service, upload documents in all functional tests before submitting a ccd case

--- src/functionalTest/java/uk/gov/hmcts/reform/wapostdeploymentfttests/services/CcdCaseCreator.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wapostdeploymentfttests/services/CcdCaseCreator.java
@@ -1,5 +1,8 @@
 package uk.gov.hmcts.reform.wapostdeploymentfttests.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import io.restassured.http.Headers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,7 +11,9 @@ import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.ccd.client.model.Event;
 import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
+import uk.gov.hmcts.reform.wapostdeploymentfttests.domain.entities.documents.Document;
 import uk.gov.hmcts.reform.wapostdeploymentfttests.domain.entities.idam.UserInfo;
+import uk.gov.hmcts.reform.wapostdeploymentfttests.preparers.DocumentManagementFiles;
 import uk.gov.hmcts.reform.wapostdeploymentfttests.util.MapMerger;
 import uk.gov.hmcts.reform.wapostdeploymentfttests.util.MapSerializer;
 import uk.gov.hmcts.reform.wapostdeploymentfttests.util.MapValueExpander;
@@ -21,6 +26,7 @@ import java.util.Map;
 import java.util.Objects;
 
 import static java.util.Collections.emptyMap;
+import static uk.gov.hmcts.reform.wapostdeploymentfttests.domain.entities.documents.DocumentNames.NOTICE_OF_APPEAL_PDF;
 import static uk.gov.hmcts.reform.wapostdeploymentfttests.services.AuthorizationHeadersProvider.AUTHORIZATION;
 import static uk.gov.hmcts.reform.wapostdeploymentfttests.services.AuthorizationHeadersProvider.SERVICE_AUTHORIZATION;
 
@@ -32,6 +38,8 @@ public class CcdCaseCreator {
     protected AuthorizationHeadersProvider authorizationHeadersProvider;
     @Autowired
     private MapValueExpander mapValueExpander;
+    @Autowired
+    private DocumentManagementFiles documentManagementFiles;
 
     public String createCase(Map<String, Object> scenario,
                              String jurisdiction,
@@ -92,10 +100,10 @@ public class CcdCaseCreator {
         CaseDataContent caseDataContent = CaseDataContent.builder()
             .eventToken(startCase.getToken())
             .event(Event.builder()
-                .id(startCase.getEventId())
-                .summary("summary")
-                .description("description")
-                .build())
+                       .id(startCase.getEventId())
+                       .summary("summary")
+                       .description("description")
+                       .build())
             .data(caseData)
             .build();
 
@@ -145,10 +153,10 @@ public class CcdCaseCreator {
         CaseDataContent submitCaseDataContent = CaseDataContent.builder()
             .eventToken(startEventResponse.getToken())
             .event(Event.builder()
-                .id(startEventResponse.getEventId())
-                .summary("summary")
-                .description("description")
-                .build())
+                       .id(startEventResponse.getEventId())
+                       .summary("summary")
+                       .description("description")
+                       .build())
             .data(caseData)
             .build();
 
@@ -191,7 +199,12 @@ public class CcdCaseCreator {
 
         String templateFilename = MapValueExtractor.extract(caseDataInput, "template");
 
-        Map<String, Object> caseData = deserializeWithExpandedValues(templatesByFilename.get(templateFilename));
+        String template = templatesByFilename.get(templateFilename);
+
+        Document noticeOfAppealDocument = documentManagementFiles.getDocument(NOTICE_OF_APPEAL_PDF);
+        template = template.replace("\"{$NOTICE_OF_DECISION_DOCUMENT}\"", toJsonString(noticeOfAppealDocument));
+
+        Map<String, Object> caseData = deserializeWithExpandedValues(template);
         Map<String, Object> caseDataReplacements = MapValueExtractor.extract(caseDataInput, "replacements");
         if (caseDataReplacements != null) {
             MapMerger.merge(caseData, caseDataReplacements);
@@ -206,4 +219,15 @@ public class CcdCaseCreator {
         return data;
     }
 
+    private String toJsonString(Object object) {
+        String json = null;
+
+        try {
+            json = new ObjectMapper().writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return json;
+    }
 }
