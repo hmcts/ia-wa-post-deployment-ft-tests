62228bb447656997faffeb7ca02467d6eb02dd7e
nilay <nilay.patel@solirius.com>
2025-07-23 15:44:32 +0100

    debug improvement?

--- src/functionalTest/java/uk/gov/hmcts/reform/wapostdeploymentfttests/SpringBootFunctionalBaseTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wapostdeploymentfttests/SpringBootFunctionalBaseTest.java
@@ -12,7 +12,7 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("functional")
 public abstract class SpringBootFunctionalBaseTest {
 
-    public static final int DEFAULT_TIMEOUT_SECONDS = 30;
+    public static final int DEFAULT_TIMEOUT_SECONDS = 300;
     public static final int DEFAULT_POLL_INTERVAL_SECONDS = 4;
     @Autowired
     protected ObjectMapper objectMapper;
